70a0311aa0cbf2c1f38ce177139d37990d40489d
Gabriel Gioscia Velloso <51892326+ggvell@users.noreply.github.com>
2024-05-26 20:14:19 +0000

    adjust to home-image-carousel

--- apps/frontend/src/features/warns/components/home-image-carousel.tsx
+++ apps/frontend/src/features/warns/components/home-image-carousel.tsx
@@ -1,29 +1,15 @@
-import { Link } from 'react-router-dom';
-import { Typography } from '../../../components/Typography';
-import { routes } from '../../../router/routes';
-import { Icon } from '../../../components/icons';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { useHomeImages } from '../hooks';
-import { WarnCarouselItem } from './warn-carousel-item';
 import { Loading } from '../../../components/loading';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import React from 'react';
+import { HomeImageCarouselItem } from './home-image-carousel-item';
 
 export function HomeImageCarousel() {
-  const { data: warns, isLoading } = useHomeImages();
+  const { data: images, isLoading } = useHomeImages();
 
   return (
     <div className="flex flex-col gap-3">
-      <div className="flex justify-between">
-        <Typography size="h2" className="font-bold">
-          Últimos Alertas
-        </Typography>
-        <Link to={routes.WARNS()} className="flex font-semibold gap-1 text-sm items-center">
-          ver todos
-          <Icon className="text-primary-500" type="chevronRight" size={7} />
-        </Link>
-      </div>
       {isLoading ? (
         <div className="flex justify-center">
           <Loading />
@@ -31,10 +17,10 @@ export function HomeImageCarousel() {
       ) : (
         <div className="flex">
           <Swiper className="w-full h-full overflow-visible" slidesPerView="auto" spaceBetween={16} allowTouchMove>
-            {warns?.map((warn) => {
+            {images?.map((image) => {
               return (
-                <SwiperSlide className="w-[200px] h-[202px]" key={warn.alertId}>
-                  <WarnCarouselItem id={warn.alertId} title={warn.title} imgLink={warn.imageUrl} />
+                <SwiperSlide className="w-[200px] h-[202px]">
+                  <HomeImageCarouselItem extLink={image.imageUrl} imgLink={image.imageUrl} />
                 </SwiperSlide>
               );
             })}
--- apps/frontend/src/features/warns/hooks/use-home-images.ts
+++ apps/frontend/src/features/warns/hooks/use-home-images.ts
@@ -1,12 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
-import { findAllWarns } from '../api';
+import { findHomeImages } from '../api';
 import { useAuth } from '../../auth';
 
 export const useHomeImages = () => {
   const { authenticated } = useAuth();
   return useQuery({
-    queryKey: ['warns'],
-    queryFn: () => findAllWarns(),
+    queryKey: ['images'],
+    queryFn: () => findHomeImages(),
     enabled: authenticated,
   });
 };
